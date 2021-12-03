23aea4e183785019f6dccdb01e48ee8f4c7fe7db
Chequi10 <acerboezequiel@live.com>
2021-12-03 19:21:33 -0300

    uart

--- Practica4/Drivers/API/Src/API_uart.c
+++ Practica4/Drivers/API/Src/API_uart.c
@@ -8,29 +8,24 @@
 
 bool_t uartInit(){
 
+	UartHandle.Instance        = USART3;
+	UartHandle.Init.BaudRate   = 9600;
+	UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
+	UartHandle.Init.StopBits   = UART_STOPBITS_1;
+	UartHandle.Init.Parity     = UART_PARITY_NONE;
+	UartHandle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
+	UartHandle.Init.Mode       = UART_MODE_TX_RX;
+	UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
 
-	  UartHandle.Instance        = USART3;
+	bool_t EstadoUart;
+    uint8_t miString[] = "BaudRate = 9600;\n\rData bits = 8;\n\rStopBits = 1;\n\rParity=NONE \n\r";
 
-	  UartHandle.Init.BaudRate   = 9600;
-	  UartHandle.Init.WordLength = UART_WORDLENGTH_8B;
-	  UartHandle.Init.StopBits   = UART_STOPBITS_1;
-	  UartHandle.Init.Parity     = UART_PARITY_NONE;
-	  UartHandle.Init.HwFlowCtl  = UART_HWCONTROL_NONE;
-	  UartHandle.Init.Mode       = UART_MODE_TX_RX;
-	  UartHandle.Init.OverSampling = UART_OVERSAMPLING_16;
-
-	  bool_t EstadoUart;
-
-	  uint8_t miString[] = "BaudRate = 9600;\n\rData bits = 8;\n\rStopBits = 1;\n\rParity=NONE \n\r";
-
-
-	  if (HAL_UART_Init(&UartHandle) != HAL_OK)
-	  {
+	  if (HAL_UART_Init(&UartHandle) != HAL_OK){
 		  EstadoUart = false;
 
 	  }	else {
-		  EstadoUart = true;
 
+		  EstadoUart = true;
 		  HAL_UART_Transmit(&UartHandle, (uint8_t *) miString, sizeof(miString)/sizeof(char), 1000);
 	  }
 
@@ -41,12 +36,10 @@ bool_t uartInit(){
 
 
 void uartSendString(uint8_t *ptrstring){
-
 	uint8_t longitud=0;
 
 	while(*(ptrstring+longitud) != 0) longitud++;
-
-	HAL_UART_Transmit(&UartHandle, ptrstring, longitud, 1000);
+		HAL_UART_Transmit(&UartHandle, ptrstring, longitud, 1000);
 
 }
 
